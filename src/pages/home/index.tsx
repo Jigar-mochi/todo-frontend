70a67cdfd8143be06933512ce5cdf53d236c3a26
Jigar-mochi <jigar102024@gmail.com>
2024-12-17 14:50:14 +0530

    Added basic routes

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card } from 'primereact/card';
+
+const ToDoPage = () => {
+    return (
+        <div className='todo-container'>
+            <Card title="Simple Card">
+                <p className="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+                    numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                </p>
+            </Card>
+        </div>
+    );
+};
+
+export default ToDoPage;
