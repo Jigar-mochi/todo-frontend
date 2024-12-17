eabac449104d726f355e6748d331362817bb8d57
Jigar-mochi <jigar102024@gmail.com>
2024-12-17 12:06:51 +0530

    added eslint new additional rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -23,6 +23,18 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'indent': ['error', 4], // Enforce 4 spaces for indentation
+      'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }], // Warn on unused variables
+      'semi': ['error', 'always'], // Enforce semicolons
+      'quotes': ['error', 'single', { avoidEscape: true }], // Use single quotes
+      'eqeqeq': ['error', 'always'], // Require strict equality
+      'curly': ['error', 'all'], // Enforce consistent curly braces for control statements
+      'no-console': 'warn', // Warn on console statements
+      'no-debugger': 'error', // Disallow the use of debugger
+      'arrow-spacing': ['error', { before: true, after: true }], // Enforce spaces around arrow functions
+      'object-curly-spacing': ['error', 'always'], // Enforce spacing inside curly braces
+      'array-bracket-spacing': ['error', 'never'], // Disallow spacing inside array brackets
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }], // Enforce spacing around object literal keys and values
     },
   },
-)
+);
